eaa6f5d9e332b9b8237389ef380165da6bb81723
Lisa Vader <lisvad@biosustain.dtu.dk>
2025-08-11 12:57:29 +0200

    Add intron finding and optimise intergenic region finding

--- scripts/get_genome_features.py
+++ scripts/get_genome_features.py
@@ -11,7 +11,9 @@ def parse_gff3(filename):
                 continue
             record, source, feature, start, end, score, strand, phase, attributes = line.strip().split("\t")
             start, end = int(start), int(end)
+            assert end >= start
 
+            feature_id = re.search("(?<=ID=).+?(?=;)", attributes).group(0)
             try:
                 parent = re.search("(?<=Parent=).+?(?=;)", attributes).group(0)
             except AttributeError:
@@ -20,6 +22,7 @@ def parse_gff3(filename):
             if feature in ["region", "gene", "mRNA", "exon", "CDS"]:
                 features_by_record[record].append({
                     "feature" : feature,
+                    "id" : feature_id,
                     "start" : start,
                     "end" : end,
                     "strand" : strand,
@@ -28,40 +31,66 @@ def parse_gff3(filename):
         return features_by_record
 
 def add_intergenic(features):
-    gene_coordinates = sorted([[f["start"],f["end"]] for f in features if f["feature"] == "gene"])
-    if not gene_coordinates:
+    gene_features = [f for f in features if f["feature"] == "gene"]
+    if not gene_features:
         return features
 
-    merged = [gene_coordinates[0]]
-    for gene in gene_coordinates[1:]:
-        #If there is a distance between the start of this gene and the end of the previous
-        if gene[0] - merged[-1][1] > 1:
-            merged.append(gene)
-        else:
-            merged[-1] = [merged[-1][0], max(merged[-1][1], gene[1])]
-
-    regions = [feature for feature in features if feature["feature"] == "region"]
-    assert len(regions) == 1
-    region = regions[0]
+    gene_features = sorted(gene_features, key=lambda f: f["start"])
+    [region] = [feature for feature in features if feature["feature"] == "region"]
     assert region["start"] == 1
+    intergenic_start = 1
 
-    starts = [1]+[p[1]+1 for p in merged]
-    ends = [p[0]-1 for p in merged]+[region["end"]]
-    for start, end in zip(starts, ends):
-        if start < end:
+    for gene in gene_features:
+        intergenic_end = gene["start"]-1
+        if intergenic_end >= intergenic_start:
             features.append({
                 "feature" : "intergenic",
-                "start" : start,
-                "end" : end,
+                "id" : None,
+                "start" : intergenic_start,
+                "end" : intergenic_end,
                 "strand" : None,
                 "parent" : None,
             })
+        intergenic_start = gene["end"]+1
+
+    if region["end"] >= intergenic_start:
+        features.append({
+            "feature" : "intergenic",
+            "id" : None,
+            "start" : intergenic_start,
+            "end" : region["end"],
+            "strand" : None,
+            "parent" : None,
+        })
+    return features
+
+def add_introns(features):
+    mRNA_features = [f for f in features if f["feature"] == "mRNA"]
+    for feature in mRNA_features:
+        exons = [f for f in features if f["feature"] == "exon" and f["parent"] == feature["id"]]
+        exons = sorted(exons, key=lambda e: e["start"])
+        if len(exons) > 1:
+            (strand,) = set(exon["strand"] for exon in exons)
+            intron_start = exons[0]["end"]+1
+            for exon in exons[1:]:
+                intron_end = exon["start"]-1
+                assert intron_end >= intron_start
+                features.append({
+                    "feature" : "intron",
+                    "id" : None,
+                    "start" : intron_start,
+                    "end" : intron_end,
+                    "strand" : strand,
+                    "parent" : feature["id"],
+                })
+                intron_start = exon["end"]+1
     return features
 
 def main(gff_file, output):
     features_by_record = parse_gff3(gff_file)
     for record, features in features_by_record.items():
         features = add_intergenic(features)
+        features = add_introns(features)
     with open(output, 'w') as stream:
         header = ["record","feature","start","end","strand","parent"]
         stream.write('\t'.join(header)+'\n')
