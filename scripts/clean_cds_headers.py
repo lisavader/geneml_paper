9231beb1c75bc329f041c41f25bf7799c8e205ba
lisvad <lisvad@nisin.win.dtu.dk>
2025-08-15 14:07:26 +0200

    Deal with pseudogenes

--- scripts/clean_cds_headers.py
+++ scripts/clean_cds_headers.py
@@ -2,13 +2,21 @@ import argparse
 import re
 
 def main(input, output):
+    pseudo_count = 0
     with open(input, 'r') as s_in, open(output, 'w') as s_out:
         for line in s_in:
             if '>' in line:
-                protein_id = re.search("(?<=cds_).{5,}?(?=_)", line).group(0)
+                if "pseudo=true" in line:
+                    pseudo_count += 1
+                    continue
+                try:
+                    protein_id = re.search("(?<=cds_)[A-Z0-9_.]{6,}?(?=_)", line).group(0)
+                except AttributeError:
+                    raise ValueError(f"Invalid line: {line}")
                 s_out.write(''.join(['>',protein_id,'\n']))
             else:
                 s_out.write(line)
+    print(f"Done.\nSkipped {pseudo_count} pseudogenes.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
